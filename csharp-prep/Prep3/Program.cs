92da4623eb74daaf151b974ae9a9cfaeaa951d03
cupofvilatte <kna22003@byui.edu>
2024-05-01 12:29:20 -0600

    completed prep3 program with counter stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string magic = Console.ReadLine();
+
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 101);
+
+        int guessNumber = 0;
+        int guessCount = 0;
+
+        while (guessNumber != magicNumber)
+        {
+            guessCount ++;
+
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You got it right!");
+                Console.WriteLine($"It took you {guessCount} guesses.");
+            }
+        }
     }
 }
