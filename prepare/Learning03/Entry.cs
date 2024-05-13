6d145610110d2d53682349c091b31d077b94ffd6
cupofvilatte <kna22003@byui.edu>
2024-05-13 16:13:23 -0600

    created entry and journal files with classes

---
+++ prepare/Learning03/Entry.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+class Entry {
+    private string prompt;
+    private string response;
+    private DateOnly date;
+    public Entry(string prompt, string response, DateOnly date) {
+        // empty for now
+    }
+
+    public void DisplayEntry() {
+
+    }
+}
---
+++ prepare/Learning03/Journal.cs
@@ -0,0 +1,25 @@
+using System;
+class Journal {
+    private List<Entry> entries;
+    private List<String> prompts;
+
+    public void NewEntry() {
+
+    }
+
+    public void DeleteEntry() {
+
+    }
+
+    public void DisplayJournal() {
+
+    }
+
+    public void SaveJournal() {
+
+    }
+
+    public void LoadJournal() {
+        
+    }
+}
