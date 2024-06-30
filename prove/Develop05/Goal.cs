33c2c952af73d8135abb5384e5813e1834a9f86c
cupofvilatte <kna22003@byui.edu>
2024-06-30 04:36:36 -0600

    omit some unnecessary code

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -24,28 +24,6 @@ public abstract class Goal {
 
     public override abstract string ToString();
 
-    public static Goal LoadGoal(string goalString) {
-        string[] parts = goalString.Split(',');
-        string type = parts[0];
-        string name = parts[1];
-        string description = parts[2];
-        int value = int.Parse(parts[3]);
-        bool completed = bool.Parse(parts[4]);
-
-        switch (type)
-        {
-            case "SimpleGoal":
-                return new SimpleGoal(name, description, value) { completed = completed };
-            case "ChecklistGoal":
-                int timesNeeded = int.Parse(parts[5]);
-                int timesCompleted = int.Parse(parts[6]);
-                return new ChecklistGoal(name, description, value, timesNeeded, timesCompleted) { completed = completed};
-            case "EternalGoal":
-                return new EternalGoal(name, description, value) { completed = completed};
-            default:
-                throw new Exception("Unknown goal type");
-        }
-    }
 
     public virtual int CompleteGoal() {
         completed = true;
